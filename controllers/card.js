994894feadb0ead77654c68de1b06cc8c8045e36
YAHTY <119876639+yahty01@users.noreply.github.com>
2023-09-03 23:46:04 +0300

    fix

--- controllers/card.js
+++ controllers/card.js
@@ -58,17 +58,8 @@ module.exports.likeCard = (req, res) => {
     })
     .then((card) => res.status(200).send({ data: card }))
     .catch((error) => {
-      if (error.name === 'ValidationError') {
-        res
-          .status(400)
-          .send({ message: 'Переданы невалидные данные карточки' });
-      } else if (error.name === 'CastError') {
-        res.status(400).send({ message: 'Передан невалидный id карточки' });
-      } else if (res.statusCode === 404) {
-        res.send({ message: 'Запрашиваемая карточка не найдена' });
-      } else {
-        res.status(500).send({ message: `${error.message}` });
-      }
+      console.log(error);
+      return res.status(500).send({ message: error.message });
     });
 };
 
@@ -84,16 +75,7 @@ module.exports.dislikeCard = (req, res) => {
     })
     .then((card) => res.status(200).send({ data: card }))
     .catch((error) => {
-      if (error.name === 'ValidationError') {
-        res
-          .status(400)
-          .send({ message: 'Переданы невалидные данные карточки' });
-      } else if (error.name === 'CastError') {
-        res.status(400).send({ message: 'Передан невалидный id карточки' });
-      } else if (res.statusCode === 404) {
-        res.send({ message: 'Запрашиваемая карточка не найдена' });
-      } else {
-        res.status(500).send({ message: `${error.message}` });
-      }
+      console.log(error);
+      return res.status(500).send({ message: error.message });
     });
 };
